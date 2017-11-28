b1caa748dd63ceda66ac9009bccee8037fdcbdef
gecongcong <congcong.ge@foxmail.com>
2017-11-28 09:45:09 +0800

    update sampling algorithm

--- src/util/Sampling.java
+++ src/util/Sampling.java
@@ -1,30 +1,26 @@
 package util;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import javax.swing.text.html.parser.Entity;
+import java.io.*;
+import java.util.*;
 
 public class Sampling {
-    public static String baseURL = "/Users/gecongcong/experiment/dataSet";    // source file baseURL
-    public static String sourceFile = baseURL + "";
-    public static HashMap<String, Integer> dataMap = new HashMap<>();
+    public static String baseURL = "/home/gcc/experiment/dataSet/";    // source file baseURL
+    public static String sourceFile = baseURL + "test/source.txt";
+    public static String sampleFile = baseURL + "test/sample.txt";
+    public static HashMap<String, ArrayList<Integer>> dataMap = new HashMap<>();//存放对应这条String的所有元组ID
 
-    class dataLink { //存放对应这条String的所有元组ID
-        String keyword;
-        ArrayList<Integer> IDlist = new ArrayList<>();
-    }
-
-    public static void run(String sourceFile, int sampleNum, int[] ignoredIDs) {
+    public static void run(String sourceFile, String sampleFile, int sampleNum, int[] ignoredIDs) {
         //read data set from sourceFile
         FileReader reader;
-        ArrayList<String> dataSet = new ArrayList<>();
+        ArrayList<String> dataSet = new ArrayList<>();  //存放原始数据
+        ArrayList<String> sampleData = new ArrayList<>();  //存放采样后的数据
         try {
             reader = new FileReader(sourceFile);
-
             BufferedReader br = new BufferedReader(reader);
-            String line = null;
+            String line;
+            int lineID = 0;
+            br.readLine();   //ignore header line
             while ((line = br.readLine()) != null && line.length() != 0) {
                 dataSet.add(line);
                 String[] tuple = line.split(",");
@@ -34,30 +30,122 @@ public class Sampling {
                     for (int new_i = 0; new_i < newTuple.length; new_i++) {
                         if(new_i!=ignoredIDs[k]){
                             newTuple[new_i] = tuple[old_i];
+                            old_i++;
                         }else{
                             old_i++;
                         }
                     }
                 }
-
-                if (!dataMap.containsKey(line)) {
-                    dataMap.put(line, 1);
+                String newLine = Arrays.toString(newTuple)
+                        .replaceAll("\\[","")
+                        .replaceAll("]","")
+                        .replaceAll(" ","");
+                if (!dataMap.containsKey(newLine)) {
+                    ArrayList<Integer> linkIDs = new ArrayList<>();
+                    linkIDs.add(lineID);
+                    dataMap.put(newLine, linkIDs);
                 } else {
-                    dataMap.put(line, dataMap.get(line) + 1);
+                    ArrayList<Integer> linkIDs = dataMap.get(newLine);
+                    linkIDs.add(lineID);
+                    dataMap.put(newLine,linkIDs);
                 }
+                lineID++;
             }
+            br.close();
+            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        //do sampling
+        double ratio = (double) sampleNum / dataSet.size();
+        Iterator<Map.Entry<String, ArrayList<Integer>>> iter = dataMap.entrySet().iterator();
+        while (iter.hasNext()){
+            Map.Entry<String, ArrayList<Integer>> entry = iter.next();
+            //String keywords = entry.getKey();
+            ArrayList<Integer> linkedIDs = entry.getValue();
+            int size = linkedIDs.size();
+            int sample_size = (int)Math.round(size * ratio);
 
+            int i=0;
+            int random_result;
+            boolean[] bool = new boolean[sample_size];
+            int num=0;
+            while (i<sample_size){
+                do{
+                    random_result = linkedIDs.get(getRandomIndex(size));//采样得到random_result这个tupleID
+                }while (bool[num]);
+                num++;
+                //寻找random result对应的tuple,并存入sampleData中
+                sampleData.add(dataSet.get(random_result));
+                i++;
+            }
+        }
+        writeToFile(sampleData,sampleFile);
     }
 
     public static void main(String[] args) {
-        int sampleNum = 100;
-        int[] ignoredIDs = {2, 4};
-        run(sourceFile, sampleNum, ignoredIDs);
-        return;
+        int sampleNum = 5;
+        int[] ignoredIDs = {7};
+        run(sourceFile,sampleFile, sampleNum, ignoredIDs);
     }
 
+    public static int getRandomIndex(int size){
+        Random random = new Random();
+        int result = random.nextInt(size);
+        System.out.println("random int = " + result);
+        return result;
+    }
+
+    /**
+     * 生成n个不同的随机数，且随机数区间为[0,random_size)
+     * @param n
+     * @return
+     */
+    public ArrayList getDiffNum(int n,int random_size){
+        // 生成 [0-n) 个不重复的随机数
+        // list 用来保存这些随机数
+        ArrayList list = new ArrayList();
+        Random rand = new Random();
+        boolean[] bool = new boolean[n];
+        int num = 0;
+        for (int i = 0; i < n; i++) {
+            do {
+                // 如果产生的数相同继续循环
+                num = rand.nextInt(random_size);
+            } while (bool[num]);
+            bool[num] = true;
+            list.add(num);
+        }
+        return list;
+    }
+
+    public static void writeToFile(ArrayList<String> list, String url){
+        File file = new File(url);
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        try {
+            if (file.exists()) {// 判断文件是否存在
+                System.out.println("File url: " + url);
+            } else if (!file.getParentFile().exists()) {// 判断目标文件所在的目录是否存在
+                // 如果目标文件所在的文件夹不存在，则创建父文件夹
+                System.out.println("create file!");
+                if (!file.getParentFile().mkdirs()) {// 判断创建目录是否成功
+                    System.out.println("failed to create file");
+                }
+            } else {
+                file.createNewFile();
+            }
+            fw = new FileWriter(file);
+            bw = new BufferedWriter(fw);
+            for(String tuple: list){
+                bw.write(tuple);
+                bw.newLine();
+            }
+            bw.close();
+            fw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
