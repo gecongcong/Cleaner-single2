5524afb872e4ed3484b99d98751ebc5617ac81fa
gecongcong <congcong.ge@foxmail.com>
2018-01-03 22:34:24 +0800

    fix bug

--- src/main/SimilarTest.java
+++ src/main/SimilarTest.java
@@ -7,22 +7,43 @@ import info.debatty.java.stringsimilarity.*;
  */
 public class SimilarTest {
     public static void main(String[] args) {
-        String s1 = "36301";
-        String s2 = "36301a";
-        MetricLCS lcs = new MetricLCS();
-        NormalizedLevenshtein l = new NormalizedLevenshtein();
+        //String s1 = "tsx,amgeneral,convertible,2010,new,fwd,4,2.4li4";
+        String s1 = "rdx,amgeneral,wagon,2011,new,awd,4,2.3li4";
+        String s2 = "rdx,amgeneral,wagon,2011,new,awd,4,2.0li4";
+        String[] out_content = s1.split(",");
+        String[] curr_content = s2.split(",");
+
+        Damerau damerau = new Damerau();
         Cosine cosine = new Cosine();
         QGram qGram = new QGram();
-        JaroWinkler jw = new JaroWinkler();
-        double qgR = qGram.distance(s1,s2);
-        double lcsR = lcs.distance(s1, s2);
-        double lsR = l.distance(s1,s2);
-        double cosR = cosine.distance(s1,s2);
-        double jwR = jw.similarity(s1,s2);
-        System.out.println("lcs result="+lcsR);
-        System.out.println("NormalizedLevenshtein result="+lcsR);
-        System.out.println("cosine result="+cosR);
-        System.out.println("QGram result="+qgR);
-        System.out.println("JaroWinkler result="+jwR);
+        double qgR = qGram.distance(s1, s2);
+        double lcsR = new MetricLCS().distance(s1, s2);
+        double lsR = new NormalizedLevenshtein().distance(s1, s2);
+        double cosR = cosine.distance(s1, s2);
+        double damerauR = damerau.distance(s1, s2);
+        double jaccardR = new Jaccard().distance(s1, s2);
+        double jaroWinklerR = new JaroWinkler().distance(s1, s2);
+        double LevenshteinR = new Levenshtein().distance(s1, s2);
+        double NGramR = new NGram().distance(s1, s2);
+        double optimalStringAlignment = new OptimalStringAlignment().distance(s1, s2);
+        double sorensenDice = new SorensenDice().distance(s1,s2);
+        /*double dis=0;
+        for(int j = 0;j<out_content.length;j++){
+            if(!out_content[j].equals(curr_content[j])){
+                dis += jw.similarity(out_content[j], curr_content[j]);
+            }
+        }
+        System.out.println("dis = "+dis);*/
+        System.out.println("lcs = " + lcsR);
+        System.out.println("NormalizedLevenshtein = " + lsR);
+        System.out.println("cosine = " + cosR);
+        System.out.println("QGram = " + qgR);
+        System.out.println("Damerau = " + damerauR);
+        System.out.println("Jaccard = " + jaccardR);
+        System.out.println("JaroWinkler = " + jaroWinklerR);
+        System.out.println("Levenshtein = " + LevenshteinR);
+        System.out.println("NGram = " + NGramR);
+        System.out.println("OptimalStringAlignment = " + optimalStringAlignment);
+        System.out.println("sorensenDice = " + sorensenDice);
     }
 }
---
+++ src/util/DataAnalysis.java
@@ -0,0 +1,84 @@
+package util;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Created by gcc on 17-12-31.
+ */
+public class DataAnalysis {
+    public static String baseURL = "/home/gcc/experiment/dataSet/";
+    public static String cleanFile_hasID = baseURL + "synthetic-car/ground_sampleData.csv";
+    public static String dirtyFile_hasID = baseURL + "RDBSCleaner_cleaned.txt";
+    public static String outFile = baseURL + "synthetic-car/WhereIsDirtyData.csv";
+
+    public static void main(String[] args) {
+        ArrayList<String[]> cleanList = new ArrayList<>();
+        ArrayList<String[]> dirtyList = new ArrayList<>();
+        try {
+            FileReader file = new FileReader(cleanFile_hasID);
+            BufferedReader br = new BufferedReader(file);
+            br.readLine();  //header
+            String str;
+            while ((str = br.readLine()) != null && str.length() != 0) {
+                String[] splitStr = str.split(",");
+                cleanList.add(splitStr);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileReader file = new FileReader(dirtyFile_hasID);
+            BufferedReader br = new BufferedReader(file);
+            br.readLine();  //header
+            String str;
+            while ((str = br.readLine()) != null && str.length() != 0) {
+                String[] splitStr = str.split(",");
+                dirtyList.add(splitStr);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        class Tmp {
+            String tupleID;
+            int AttrIndex;
+            String clean_value;
+            String dirty_value;
+        }
+        ArrayList<Tmp> list = new ArrayList<>();
+        for (int i = 0; i < cleanList.size(); i++) {
+            String[] clean_curr = cleanList.get(i);
+            String[] dirty_curr = dirtyList.get(i);
+            for (int k = 1; k < clean_curr.length; k++) { //ignore k=0, because it is Tuple ID
+                if (!clean_curr[k].equals(dirty_curr[k])) {
+                    Tmp tmp = new Tmp();
+                    tmp.tupleID = clean_curr[0];
+                    tmp.AttrIndex = k;
+                    tmp.clean_value = clean_curr[k];
+                    tmp.dirty_value = dirty_curr[k];
+                    list.add(tmp);
+                }
+            }
+        }
+
+        try {
+            FileWriter fw = new FileWriter(outFile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (int i = 0; i < list.size(); i++) {
+                Tmp tmp = list.get(i);
+                String tupleID = tmp.tupleID;
+                int AttrIndex = tmp.AttrIndex;
+                String clean_value = tmp.clean_value;
+                String dirty_value = tmp.dirty_value;
+                bw.write("ID = " + tupleID + "; AttrIndex = " + AttrIndex + "; clean_value = " + clean_value + "; dirty_value = " + dirty_value);
+                bw.newLine();
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
